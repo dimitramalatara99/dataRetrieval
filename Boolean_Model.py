c7f331a804551b46df2d68576b26d77b7d3bbbbf
ElinaMaraki <134548493+ElinaMaraki@users.noreply.github.com>
2025-01-20 03:05:10 +0200

    Add files via upload

--- Boolean_Model.py
+++ Boolean_Model.py
@@ -2,19 +2,6 @@ class BooleanModel:
     def __init__(self, boolean_inv_index):
         self.boolean_inv_index = boolean_inv_index
 
-    def get_documents(self, term):
-        return self.boolean_inv_index.get(term, set())
-
-    def handle_not(self, term_set):
-        all_docs = self.get_all_documents()
-        return all_docs - term_set
-
-    def get_all_documents(self):
-        all_docs = set()
-        for docs in self.boolean_inv_index.values():
-            all_docs.update(docs)
-        return all_docs
-
     def process_query(self, query):
         tokens1 = query.lower().split()
         test = ""
